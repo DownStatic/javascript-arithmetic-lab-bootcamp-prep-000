c53006dbc15348716eabc2fa74df7e5868bed52b
Christopher Stull <chris.stull@gmail.com>
2018-05-22 18:14:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function subtract(a,b){
   return a-b
 }
 
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function inc(n){
+  n++
+  return n
+}
+
+function dec(n){
+  n--
+  return n
+}
